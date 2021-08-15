32dd8b3e8d76f54f2371a26735578761057d71eb
Casey Tucker <dctucker@hotmail.com>
2021-08-15 13:26:36 -0700

    more documentation, main.py support STDIN

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from sys import argv
+import fileinput
 from roland_sysex import *
 
 #filename = 'mixer/mutes-all.txt'
@@ -8,13 +8,11 @@ from roland_sysex import *
 #filename = 'mixer/some-monitors.txt'
 #filename = 'mixer/widgets.txt'
 #filename = 'mixer/pan-1a.txt'
-filename = argv[1]
 packets = []
-with open(filename) as f:
-	for line in f.readlines():
-		line = line.strip()
-		packet = [int(line[i:i+2],16) for i in range(0, len(line), 2)]
-		packets.extend([packet])
+for line in fileinput.input():
+	line = line.rstrip()
+	packet = [int(line[i:i+2],16) for i in range(0, len(line), 2)]
+	packets.extend([packet])
 
 #from mixer.device_volume_1a import packets
 process_packets(packets)
