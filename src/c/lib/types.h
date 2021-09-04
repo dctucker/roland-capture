d7460c853b4e54da5d99fb2ee5e8d4414bea54aa
Casey Tucker <dctucker@hotmail.com>
2021-09-04 01:21:06 -0700

    adding C memory map

---
+++ src/c/lib/types.h
@@ -0,0 +1,24 @@
+typedef uint8_t u8;
+typedef uint32_t u32;
+typedef struct Boolean {
+	u8 value;
+} Boolean;
+typedef struct Volume {
+	u8 value[6];
+} Volume;
+typedef struct Pan {
+	u8 value[4];
+} Pan;
+
+typedef union input_channel {
+	struct {
+		Boolean  stereo;
+		u8 _unused;
+		Boolean  solo;
+		Boolean  mute;
+		Pan      pan;
+		Volume   volume;
+		Volume   reverb;
+	};
+	u8 bytes[20];
+} input_channel;
