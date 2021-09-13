a66e45050fe3e29b13486ae87ab0402cffaf04b7
Casey Tucker <dctucker@hotmail.com>
2021-09-12 17:24:46 -0700

    working seq comm implementation

--- src/c/lib/comm.c
+++ src/c/lib/comm.c
@@ -1,77 +1,113 @@
 #include "lib/comm.h"
+#include <poll.h>
 #include <alsa/asoundlib.h>
 
-static snd_rawmidi_t *midi_in;
-static snd_rawmidi_t *midi_out;
+static snd_seq_t *seq;
 
-const char *port_name = "hw:CARD=STUDIOCAPTURE,DEV=0,SUBDEV=1";
+#define TRY_SEQ(X, MSG) if( (err = X) < 0 ){ warn( MSG ": %s", snd_strerror(err) ); return 0; }
+#define MIDI_BYTES_PER_SEC (31250 / (1 + 8 + 2))
+
+//const char *port_name = "hw:CARD=STUDIOCAPTURE,DEV=0,SUBDEV=1";
+const char *port_name = "24:1";
 static unsigned char buf[8192];
 static unsigned int msglen = 0;
 static unsigned int sysex_start = sizeof(buf);
 static void (*capmix_listener)(uint8_t *, int) = NULL;
+static int output_queue;
+static int port_in, port_out;
+
+int n_descriptors;
+struct pollfd *descriptors;
 
 int capmix_setup_midi( void (*listener)(uint8_t *, int) )
 {
 	int err;
-	if( (err = snd_rawmidi_open(&midi_in, &midi_out, port_name,
-			SND_RAWMIDI_APPEND | SND_RAWMIDI_NONBLOCK | SND_RAWMIDI_SYNC
-	  )) < 0 )
-	{
-		midi_in = NULL;
-		midi_out = NULL;
-		return 0;
-	}
+	TRY_SEQ( snd_seq_open(&seq, "default", SND_SEQ_OPEN_DUPLEX, 0), "Unable to open sequencer")
+	TRY_SEQ( snd_seq_set_client_name(seq, "aseqdump")             , "Unable to set client name")
+
+	//input port
+	TRY_SEQ( snd_seq_create_simple_port(seq, "libcapmix",
+		 SND_SEQ_PORT_CAP_WRITE | SND_SEQ_PORT_CAP_SUBS_WRITE,
+		 SND_SEQ_PORT_TYPE_MIDI_GENERIC |
+		 SND_SEQ_PORT_TYPE_APPLICATION) , "Unable to create port")
+	port_in = err;
+
+	//output port
+	TRY_SEQ( snd_seq_create_simple_port(seq, "libcapmix",
+		 SND_SEQ_PORT_CAP_READ | SND_SEQ_PORT_CAP_SUBS_READ,
+		 SND_SEQ_PORT_TYPE_MIDI_GENERIC |
+		 SND_SEQ_PORT_TYPE_APPLICATION) , "Unable to create port")
+	port_out = err;
+	
+	//connect application to device
+	static snd_seq_addr_t port;
+	TRY_SEQ( snd_seq_parse_address(seq, &port, port_name)                , "Port not found")
+	TRY_SEQ( snd_seq_connect_from(seq, port_in, port.client, port.port)  , "Unable to connect input port")
+	TRY_SEQ( snd_seq_connect_to(  seq, port_out, port.client, port.port) , "Unable to connect output port")
+	TRY_SEQ( snd_seq_nonblock(seq, 1)                                    , "Unable to set nonblock mode")
+
+	//setup poll descriptors for read
+	n_descriptors = snd_seq_poll_descriptors_count(seq, POLLIN);
+	descriptors = malloc(sizeof(*descriptors) * n_descriptors);
+
     capmix_listener = listener;
-	warn("Opened %s (%x, %x)\n", port_name, midi_in, midi_out);
+	warn("Opened %s\n", port_name);
 	return 1;
 }
 
 int capmix_read_midi()
 {
 	int err;
-	err = snd_rawmidi_read(midi_in, &buf[msglen], sizeof(buf)-msglen);
-	if( err <= 0 ) return err;
-	//warn("received %d\n", err);
-	for(int i=msglen; i < msglen + err; i++)
+	snd_seq_poll_descriptors(seq, descriptors, n_descriptors, POLLIN);
+	if (poll(descriptors, n_descriptors, -1) < 0)
+		return 0;
+	do
 	{
-		//printf("0x%02x ", buf[i]);
-		if( buf[i] == 0xf0 )
-		{
-			sysex_start = i;
-		}
-		else if( buf[i] == 0xf7 )
-		{
-			msglen += err;
-			if( msglen + sysex_start < sizeof(buf) )
-				capmix_listener(&buf[sysex_start], 1 + i - sysex_start);
-			sysex_start = sizeof(buf);
+		snd_seq_event_t *event;
+		err = snd_seq_event_input(seq, &event);
+		if (err < 0)
+			return 0;
+		if( event == NULL )
+			continue;
 
-			return i;
-		}
+		if( event->type != SND_SEQ_EVENT_SYSEX )
+			continue;
+
+		/*
+		for (int i = 0; i < event->data.ext.len; i++)
+			printf(" %02X", ((unsigned char*)event->data.ext.ptr)[i]);
+		printf("\n");
+		*/
+
+		capmix_listener( event->data.ext.ptr, event->data.ext.len );
+		return event->data.ext.len;
 	}
-	printf("\n");
-	msglen += err;
-	if( msglen >= sizeof(buf) )
-	{
-		msglen = 0;
-		sysex_start = sizeof(buf);
-	}
-	return msglen;
+	while (err > 0);
+	fflush(stdout);
 }
 
 int capmix_send_midi(uint8_t *data, int len)
 {
 	int err;
-	if( (err = snd_rawmidi_write(midi_out, data, len)) < 0 )
-	{
-		warn("cannot send data: %s", snd_strerror(err));
-		return 0;
-	}
-	return err;
+	snd_seq_event_t event;
+	snd_seq_ev_clear( &event );
+	snd_seq_ev_set_source( &event, port_out );
+	snd_seq_ev_set_subs( &event );
+	snd_seq_ev_set_direct( &event );
+	snd_seq_ev_set_sysex( &event, len, data );
+
+	TRY_SEQ( snd_seq_event_output( seq, &event ), "Error sending MIDI data" )
+	snd_seq_drain_output( seq );
+
+	return 0;
 }
 
 void capmix_cleanup_midi()
 {
-	if( midi_out ) snd_rawmidi_close(midi_out);
-	if( midi_in )  snd_rawmidi_close(midi_in);
+	int err;
+	err = snd_seq_drain_output(seq);
+	err = snd_seq_sync_output_queue(seq);
+
+	snd_seq_close(seq);
+	free(descriptors);
 }
