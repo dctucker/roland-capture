8172e9b8910ddb81e6c005fe924d8648b618c05b
Casey Tucker <dctucker@hotmail.com>
2022-10-10 23:25:46 -0700

    comment out capmix_type_parent for now

--- app/list-memory.c
+++ app/list-memory.c
@@ -1,7 +1,13 @@
 #include "lib/capture.h"
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
 	capmix_set_model(MStudio);
 	capmix_print_map(NULL, "", 0);
+
+	char name[64];
+	capmix_format_addr(0x8100d, name);
+	capmix_type_t type = capmix_addr_type(0x8100d);
+	printf("name: %s, type: %d\n", name, type);
 }
